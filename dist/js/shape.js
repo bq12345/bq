4c0152bc50771fa6b8471924d11ac2b958aa4807
BaiQiang <1075707996@qq.com>
2015-03-14 18:13:31 +0800

    update

--- dist/js/shape.js
+++ dist/js/shape.js
@@ -241,7 +241,7 @@ S.UI = (function () {
         input.addEventListener('change', checkInputWidth);
         input.addEventListener('focus', checkInputWidth);
 
-       
+
 
         canvas.addEventListener('click', function (e) {
             overlay.classList.remove('overlay--visible');
@@ -264,46 +264,6 @@ S.UI = (function () {
     }
 }());
 
-
-S.UI.Tabs = (function () {
-    var tabs = document.querySelector('.tabs'),
-        labels = document.querySelector('.tabs-labels'),
-        triggers = document.querySelectorAll('.tabs-label'),
-        panels = document.querySelectorAll('.tabs-panel');
-
-    function activate(i) {
-        triggers[i].classList.add('tabs-label--active');
-        panels[i].classList.add('tabs-panel--active');
-    }
-
-    function bindEvents() {
-        labels.addEventListener('click', function (e) {
-            var el = e.target,
-                index;
-
-            if (el.classList.contains('tabs-label')) {
-                for (var t = 0; t < triggers.length; t++) {
-                    triggers[t].classList.remove('tabs-label--active');
-                    panels[t].classList.remove('tabs-panel--active');
-
-                    if (el === triggers[t]) {
-                        index = t;
-                    }
-                }
-
-                activate(index);
-            }
-        });
-    }
-
-    function init() {
-        activate(0);
-        bindEvents();
-    }
-
-    // Init
-    init();
-}());
 
 
 S.Point = function (args) {
